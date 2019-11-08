4c970621b9815fb59cb766c75e7c9eb0504403f8
ajohnson1031 <aaron.johnson1031@gmail.com>
2019-11-08 00:12:14 -0500

    added post endpoints

--- index.js
+++ index.js
@@ -51,7 +51,15 @@ const validatePost = (req, res, next) => {
     ? res.status(400).json({ message: "missing post data" })
     : !req.body.text
     ? res.status(400).json({ message: "missing required text field" })
-    : next();
+    : !req.body.user_id
+    ? res.status(400).json({ message: "missing required user_id field" })
+    : userDB
+        .getById(req.body.user_id)
+        .then(user =>
+          !user
+            ? res.status(400).json({ message: "This user does not exist" })
+            : next()
+        );
 };
 
 server.use(express.json());
@@ -61,7 +69,7 @@ server.get("/", logger, (req, res) => {
   res.status(200).send("Welcome to my Users App!");
 });
 
-//Endpoints
+//Endpoints for USERS
 
 server.get("/users", logger, (req, res) => {
   userDB
@@ -100,4 +108,37 @@ server.put("/users/:id", logger, validateUserId, (req, res) => {
 
 server.delete("/users/:id", logger, validateUserId, (req, res) => {
   userDB.remove(req.user).then(() => res.status(204).end());
+});
+
+//Endpoints for POSTS
+
+server.get("/posts", logger, (req, res) => {
+  postDB
+    .get()
+    .then(posts => res.status(200).json({ posts: posts }))
+    .catch(err =>
+      res.status(500).json({ error: "Could not retrieve posts from database." })
+    );
+});
+
+server.get("/posts/:id", logger, validateUserId, (req, res) => {
+  postDB
+    .getById(req.user)
+    .then(posts => res.status(200).json({ posts: posts }))
+    .catch(err =>
+      res
+        .status(500)
+        .json({ error: "There was an error retrieving this user's posts." })
+    );
+});
+
+server.post("/posts", logger, validatePost, (req, res) => {
+  postDB
+    .insert(req.body)
+    .then(post => res.status(201).json({ post: post }))
+    .catch(err =>
+      res
+        .status(500)
+        .json({ error: "The post could not be created at this time." })
+    );
 });
